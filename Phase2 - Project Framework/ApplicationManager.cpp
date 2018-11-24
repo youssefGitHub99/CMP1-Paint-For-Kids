e562728f5e245180a8fb7a1c7ec735eedb2591d6
Yaamani <35202399+Yaamani@users.noreply.github.com>
2018-11-24 14:53:19 +0200

    Save Action and Load Action

--- Phase2 - Project Framework/ApplicationManager.cpp
+++ Phase2 - Project Framework/ApplicationManager.cpp
@@ -1,6 +1,9 @@
+#pragma once
 #include "ApplicationManager.h"
 #include "Actions\AddRectAction.h"
 
+
+using namespace std;
 
 //Constructor
 ApplicationManager::ApplicationManager()
@@ -8,12 +11,12 @@ ApplicationManager::ApplicationManager()
 	//Create Input and output
 	pOut = new Output;
 	pIn = pOut->CreateInput();
-	
+
 	FigCount = 0;
-		
+
 	//Create an array of figure pointers and set them to NULL		
-	for(int i=0; i<MaxFigCount; i++)
-		FigList[i] = NULL;	
+	for (int i = 0; i < MaxFigCount; i++)
+		FigList[i] = NULL;
 }
 
 //==================================================================================//
@@ -22,37 +25,44 @@ ApplicationManager::ApplicationManager()
 ActionType ApplicationManager::GetUserAction() const
 {
 	//Ask the input to get the action from the user.
-	return pIn->GetUserAction();		
+	return pIn->GetUserAction();
 }
 ////////////////////////////////////////////////////////////////////////////////////
 //Creates an action and executes it
-void ApplicationManager::ExecuteAction(ActionType ActType) 
+void ApplicationManager::ExecuteAction(ActionType ActType)
 {
 	Action* pAct = NULL;
-	
+
 	//According to Action Type, create the corresponding action object
 	switch (ActType)
 	{
-		case DRAW_RECT:
-			pAct = new AddRectAction(this);
-			break;
+	case LOAD:
+		cout << SaveLoadUtility::fileDialog(DIALOG_OPEN);
+		break;
 
-		case DRAW_LINE:
-			///create AddLineAction here
+	case SAVE:
+		cout << SaveLoadUtility::fileDialog(DIALOG_SAVE);
+		break;
+	case DRAW_RECT:
+		pAct = new AddRectAction(this);
+		break;
 
-			break;
+	case DRAW_LINE:
+		///create AddLineAction here
 
-		case EXIT:
-			///create ExitAction here
-			
-			break;
-		
-		case STATUS:	//a click on the status bar ==> no action
-			return;
+		break;
+
+	case EXIT:
+		///create ExitAction here
+
+		break;
+
+	case STATUS:	//a click on the status bar ==> no action
+		return;
 	}
-	
+
 	//Execute the created action
-	if(pAct != NULL)
+	if (pAct != NULL)
 	{
 		pAct->Execute();//Execute
 		delete pAct;	//Action is not needed any more ==> delete it
@@ -66,8 +76,8 @@ void ApplicationManager::ExecuteAction(ActionType ActType)
 //Add a figure to the list of figures
 void ApplicationManager::AddFigure(CFigure* pFig)
 {
-	if(FigCount < MaxFigCount )
-		FigList[FigCount++] = pFig;	
+	if (FigCount < MaxFigCount)
+		FigList[FigCount++] = pFig;
 }
 ////////////////////////////////////////////////////////////////////////////////////
 CFigure *ApplicationManager::GetFigure(int x, int y) const
@@ -87,24 +97,29 @@ CFigure *ApplicationManager::GetFigure(int x, int y) const
 
 //Draw all figures on the user interface
 void ApplicationManager::UpdateInterface() const
-{	
-	for(int i=0; i<FigCount; i++)
+{
+	for (int i = 0; i < FigCount; i++)
 		FigList[i]->Draw(pOut);		//Call Draw function (virtual member fn)
 }
 ////////////////////////////////////////////////////////////////////////////////////
 //Return a pointer to the input
 Input *ApplicationManager::GetInput() const
-{	return pIn; }
+{
+	return pIn;
+}
 //Return a pointer to the output
 Output *ApplicationManager::GetOutput() const
-{	return pOut; }
+{
+	return pOut;
+}
 ////////////////////////////////////////////////////////////////////////////////////
 //Destructor
 ApplicationManager::~ApplicationManager()
 {
-	for(int i=0; i<FigCount; i++)
+	for (int i = 0; i < FigCount; i++)
 		delete FigList[i];
 	delete pIn;
 	delete pOut;
-	
+
 }
+
--- Phase2 - Project Framework/ApplicationManager.h
+++ Phase2 - Project Framework/ApplicationManager.h
@@ -5,6 +5,7 @@
 #include "Figures\CFigure.h"
 #include "GUI\input.h"
 #include "GUI\output.h"
+#include "SaveLoadUtility.h"
 
 //Main class that manages everything in the application.
 class ApplicationManager
@@ -39,6 +40,7 @@ public:
 	Input *GetInput() const; //Return pointer to the input
 	Output *GetOutput() const; //Return pointer to the output
 	void UpdateInterface() const;	//Redraws all the drawing window	
+
 };
 
 #endif
